12b6ad2ff37bf2bdd08f0d2c29c1b8a6282fbcc1
Francesco <cantacell@gmail.com>
2023-10-30 17:25:20 +0100

    fix(task-6): changed bff API endpoint

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -10,7 +10,7 @@ export const environment: Config = {
     product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
     order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
     import: 'https://7idvptrw76.execute-api.us-east-1.amazonaws.com/dev',
-    bff: 'https://05u37953kfexecute-api.us-east-1.amazonaws.com/dev',
+    bff: 'https://hkyr142tth.execute-api.us-east-1.amazonaws.com/dev',
     cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   },
   apiEndpointsEnabled: {
